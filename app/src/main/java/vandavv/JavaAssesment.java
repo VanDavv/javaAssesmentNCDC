9110f35516289a08a728a6701c1f3f303409c027
VanDavv <lukpila29@gmail.com>
2016-07-08 20:01:31 +0200

    Setting up basic server configuration with migrations

--- app/src/main/java/vandavv/JavaAssesment.java
+++ app/src/main/java/vandavv/JavaAssesment.java
@@ -1,4 +1,51 @@
 package vandavv;
 
-public class JavaAssesment {
+import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.jdbi.DBIFactory;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.skife.jdbi.v2.DBI;
+
+public class JavaAssesment extends Application<JavaAssesmentConfiguration> {
+
+    private static final String APP_NAME = "JavaAssesment";
+    private static final String HEALTH_CHECK_DATABASE_NAME = "database";
+
+    public static void main(String[] args) throws Exception {
+        new JavaAssesment().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return APP_NAME;
+    }
+
+    @Override
+    public void initialize(Bootstrap<JavaAssesmentConfiguration> bootstrap) {
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(
+                        bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
+
+        bootstrap.addBundle(new MigrationsBundle<JavaAssesmentConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(JavaAssesmentConfiguration configuration) {
+                return configuration.getDatabase();
+            }
+        });
+    }
+
+    @Override
+    public void run(JavaAssesmentConfiguration configuration, Environment environment) throws Exception {
+
+        final DBI jdbi = new DBIFactory().build(environment, configuration.getDatabase(), HEALTH_CHECK_DATABASE_NAME);
+
+    }
 }
---
+++ app/src/main/java/vandavv/JavaAssesmentConfiguration.java
@@ -0,0 +1,18 @@
+package vandavv;
+
+import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+public class JavaAssesmentConfiguration extends Configuration {
+
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    public DataSourceFactory getDatabase() {
+        return database;
+    }
+}
